188e3b91ac65b1318fa5c11873108ce1c65c0e4a
kmd-shafi <shaikshafi5259@gmail.com>
2024-02-13 13:52:16 +0530

    install parcel

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", { id: "react" }, "Hello REACT!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
